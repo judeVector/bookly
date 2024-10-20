2c02a3a9b802d12686c39cf50487e52a4d0549bb
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-10-20 20:53:58 +0400

    Create schemas for authentication routes

---
+++ src/auth/schemas.py
@@ -0,0 +1,29 @@
+import uuid
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
+
+class UserCreateModel(BaseModel):
+    username: str = Field(max_length=20)
+    email: str = Field(max_length=40)
+    password: str = Field(min_length=6)
+    first_name: str = Field(max_length=20)
+    last_name: str = Field(max_length=20)
+
+
+class UserModel(BaseModel):
+    uid: uuid.UUID
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    is_verified: bool
+    password_hash: str = Field(exclude=True)
+    created_at: datetime
+    updated_at: datetime
+
+
+class UserLoginModel(BaseModel):
+    email: str = Field(max_length=40)
+    password: str = Field(min_length=6)
