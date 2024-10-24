f99c34799993b19461af48c7109a752e21003417
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-10-24 20:48:13 +0400

    Add a new exception to handle invalid token

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -1,10 +1,12 @@
 import uuid
 import logging
+from loguru import logger
 
 from datetime import timedelta, datetime
 from passlib.context import CryptContext
 from src.config import Config
 import jwt
+from fastapi import HTTPException, status
 
 ACCESS_TOKEN_EXPIRY = 3600
 
@@ -51,6 +53,15 @@ def decode_token(token: str) -> dict:
             jwt=token, key=Config.JWT_SECRET, algorithms=Config.JWT_ALGORITHM
         )
         return token_data
+    except jwt.DecodeError:
+        logger.error("Invalid token or token expired")
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Invalid token or token expired",
+        )
     except jwt.PyJWTError as e:
-        logging.exception(e)
-        return None
+        logger.exception("JWT error occurred: %s", e)
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Token decoding failed",
+        )
