cda137d88925deff9e1cdef05367274d6fcc3df6
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-10-26 01:58:54 +0400

    Add endpoint for getting a perticular user

--- src/auth/routes.py
+++ src/auth/routes.py
@@ -6,13 +6,20 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from .schemas import UserCreateModel, UserLoginModel, UserModel
 from .service import UserService
 from .utils import create_access_token, decode_token, verify_password
-from .dependencies import RefreshTokenBearer, AccessTokenBearer
+from .dependencies import (
+    RefreshTokenBearer,
+    AccessTokenBearer,
+    get_current_user,
+    Rolechecker,
+)
+
 from src.db.postgres import get_session
 from src.db.redis import add_jti_to_blocklist
 
 
 auth_router = APIRouter()
 user_service = UserService()
+role_checker = Rolechecker(["admin"])
 
 # Number of days for which refresh token remains valid
 REFRESH_TOKEN_EXPIRY = 2
@@ -90,7 +97,7 @@ async def login_user(
         )
 
     # Generate tokens
-    token_data = {"email": user.email, "user_uid": str(user.uid)}
+    token_data = {"email": user.email, "user_uid": str(user.uid), "role": user.role}
     access_token = create_access_token(user_data=token_data)
     refresh_token = create_access_token(
         user_data=token_data,
@@ -136,6 +143,14 @@ async def get_new_access_token(token_details: dict = Depends(RefreshTokenBearer(
     raise HTTPException(
         status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid or expired token"
     )
+
+
+@auth_router.get("/me")
+async def get_current_user(
+    user=Depends(get_current_user), _: bool = Depends(role_checker)
+):
+
+    return user
 
 
 @auth_router.get("/logout")
