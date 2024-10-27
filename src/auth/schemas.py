5ba9f6edfcc3a76532f346aef8e9309d37453ff3
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-10-27 18:14:55 +0400

    Update the UserModel with the books list

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,7 +1,10 @@
 import uuid
 from datetime import datetime
 
+from typing import List
 from pydantic import BaseModel, Field
+
+from src.books.schemas import BookModel
 
 
 class UserCreateModel(BaseModel):
@@ -22,6 +25,7 @@ class UserModel(BaseModel):
     password_hash: str = Field(exclude=True)
     created_at: datetime
     updated_at: datetime
+    books: List[BookModel]
 
 
 class UserLoginModel(BaseModel):
