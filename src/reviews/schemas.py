89f64155721ea12df317f1e87cb382e766369391
Jude Ndubuisi <ikechukwujudendubuisi@yahoo.com>
2024-10-29 02:26:46 +0400

    Add a new schemes for reviews

---
+++ src/reviews/schemas.py
@@ -0,0 +1,26 @@
+import uuid
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class ReviewModel(BaseModel):
+    uid: uuid.UUID
+    rating: int = Field(lt=5)
+    review_text: str
+    user_uid: Optional[uuid.UUID]
+    book_uid: Optional[uuid.UUID]
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        from_attributes = True
+
+
+class ReviewCreateModel(BaseModel):
+    rating: int = Field(lt=5)
+    review_text: str
+
+    class Config:
+        from_attributes = True
